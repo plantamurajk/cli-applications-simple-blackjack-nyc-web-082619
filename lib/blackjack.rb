67847778dbf23a07efe747e3f9512b7e18b082ee
John Plantamura <plantamurajk@gmail.com>
2019-08-22 21:50:05 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,15 +7,15 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts `Your cards add up to #{card_total}.`
+  puts "Your cards add up to #{card_total}."
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
 def end_game
